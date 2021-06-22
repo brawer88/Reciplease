5584a62db82c445dd81b8d6fc864391ad1889384
brawer88 <79695807+brawer88@users.noreply.github.com>
2021-06-22 18:05:36 -0400

    Start of Project

---
+++ Example Web Application Files/web2/Controllers/AboutUsController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace web2.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        // GET: AboutUs
+        public ActionResult Index()
+        {
+			Models.User u = new Models.User( );
+			u = u.GetUserSession( );
+			u.UID = 4; // my UID
+
+			// load with my info
+			Models.Database db = new Models.Database( );
+
+			db.GetDeveloperUserInfo( ref u );
+
+			return View( u );
+        }
+
+
+		[HttpPost]
+		public ActionResult Index( FormCollection col ) {
+
+			if ( col["btnSubmit"] == "close" )
+			{
+				return RedirectToAction( "Index", "Home" );
+			}
+
+			if ( col["btnSubmit"] == "more" )
+			{
+				return RedirectToAction( "More", new { @id = Convert.ToInt64( RouteData.Values["id"] ) } );
+			}
+			return View( );
+		}
+
+		// GET: AboutUs
+		public ActionResult More( ) {
+			Models.User u = new Models.User( );
+			u = u.GetUserSession( );
+			u.UID = 4; // my UID
+
+			// load with my info
+			Models.Database db = new Models.Database( );
+
+			db.GetDeveloperUserInfo( ref u );
+
+			return View( u );
+		}
+
+
+		[HttpPost]
+		public ActionResult More( FormCollection col ) {
+
+			if ( col["btnSubmit"] == "close" )
+			{
+				return RedirectToAction( "Index", "AboutUs" );
+			}
+
+			return View( );
+		}
+
+	}
+}
---
+++ Example Web Application Files/web2/Scripts/web2.js
@@ -0,0 +1,128 @@
+﻿
+function rateEvent(uid, id, rating) {
+	try {
+		var ajaxData = { //json structure
+			UID: uid,
+			ID: id,
+			Rating: rating
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "../../Home/RateEvent",
+			data: ajaxData,
+			success: function (returnData) {
+				var i;
+
+				//remove all "checked" stars
+				for (i = 1; i <= 5; i++) {
+					$("#rate".concat(i)).removeClass("checked");
+				}
+
+				//add updated "checked" stars
+				for (i = 1; i <= rating; i++) {
+					$("#rate".concat(i)).addClass("checked");
+				}
+			},
+			error: function (xhr) {
+				debugger;
+			}
+		});
+	}
+	catch (err) {
+		showError(err);
+	}
+}
+
+function toggleEventLike(like_location, uid, id) {
+	try {
+		var ajaxData = { //json structure
+			UID: uid,
+			ID: id
+		}
+
+		var strURL;
+		if (like_location == "index") //Home/Index
+			strURL = "../Home/ToggleEventLike";
+		else // Home/Event/22
+			strURL = "../../Home/ToggleEventLike";
+
+		$.ajax({
+			type: "POST",
+			url: strURL,
+			data: ajaxData,
+			success: function (returnData) {
+				//1 = added; 0 = removed
+				if (returnData.Status == 1) {//1 = added
+					$("#like-user-i-".concat(id)).removeClass("fal fa-thumbs-up");
+					$("#like-user-i-".concat(id)).addClass("fas fa-thumbs-up");
+					var count = 0;
+					count = parseInt($("#like-count-".concat(id)).text());
+					count += 1;
+					$("#like-count-".concat(id)).text(count);
+				}
+				else {//0 = removed
+					$("#like-user-i-".concat(id)).removeClass("fas fa-thumbs-up");
+					$("#like-user-i-".concat(id)).addClass("fal fa-thumbs-up");
+					var count = 0;
+					count = parseInt($("#like-count-".concat(id)).text());
+					count -= 1;
+					if (count < 0) count = 0;
+					$("#like-count-".concat(id)).text(count);
+				}
+			},
+			error: function (xhr) {
+				debugger;
+			}
+		});
+	}
+	catch (err) {
+		showError(err);
+	}
+}
+
+function deleteImageAjax(deleteType, uid, id) {
+	try {
+		var ajaxData = { //json structure
+			UID: uid,
+			ID: id
+		};
+
+		var strURL;
+		if (deleteType == "profile")
+			strURL = "../Profile/DeleteImage";
+		else //event
+			strURL = "../../Profile/DeleteEventImage";
+
+		$.ajax({
+			type: "POST",
+			url: strURL,
+			data: ajaxData,
+			success: function (returnData) {
+				if (returnData.Status == 1) {
+					$("#image-".concat(id)).hide();
+				}
+				else {
+					alert('Unable to remove image.');
+				}
+			},
+			error: function (xhr) {
+				debugger;
+			}
+		});
+	}
+	catch (err) {
+		showError(err);
+	}
+}
+
+
+function SetActiveMenus(iconMenu, profileMenu) {
+	try {
+		if (iconMenu != "")
+			$("#icon-bar-".concat(iconMenu)).addClass("active");
+		if (profileMenu != undefined)
+			$("#profile-".concat(profileMenu)).addClass("active");
+	}
+	catch (Exception) { /* ignore errors here */}
+}
